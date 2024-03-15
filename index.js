f4226abf4f67c91cf3a07d848673db8d1acc15a6
Maye Edwin <mayedwine1@gmail.com>
2024-03-15 09:53:48 +0000

    feat(ui) - update ui, add juice : )

--- index.js
+++ index.js
@@ -1,86 +1,84 @@
-import {GoogleGenerativeAI} from 'https://esm.run/@google/generative-ai';
+import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 
-// Fetch your API Key
-const API_KEY = 'YOUR_API_KEY';
-
-// Access your API key (see "Set up your API key" above)
+// Fetch your API Key : > config
+const API_KEY = "YOUR_API_KEY";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
-const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
-
+// Get elements from the DOM
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
 const respDialog = document.querySelector("dialog");
-const message = document.getElementById('message');
+const message = document.getElementById("message");
+const filePicker = document.getElementById("file");
+const afterPromptResponse = document.getElementById("gemini-response");
 
+// Ask gemini anything using text - text-only input
+open.addEventListener("click", async () => {
+  try {
+    const askGemini = async () => {
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const promptText = document.getElementById("gemini-prompt").value;
 
-const filePicker = document.getElementById('file');
+      const result = await model.generateContent(promptText);
+      const response = await result.response;
+      const text = response.text();
 
-
-
-open.addEventListener('click', async () => {
-    async function run() {
-        // For text-only input, use the gemini-pro model
-        const model = genAI.getGenerativeModel({model: "gemini-pro"});
-
-        const promptText = document.getElementById('gemini-prompt').value;
-
-        const result = await model.generateContent(promptText);
-        const response = await result.response;
-        const text = response.text();
-
-        const afterPromptResponse = document.getElementById('gemini-response');
-        afterPromptResponse.innerText = text;
-
-        console.log(text);
-    }
-
-    await run();
-    modal_container.classList.add('show');
+      afterPromptResponse.innerText = text;
+    };
+    await askGemini();
+    modal_container.classList.add("show");
+  } catch (error) {
+    const message = error?.message ?? "Something went wrong"
+    console.warn(message);
+    alert(message)
+  }
 });
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
 });
 
-filePicker.addEventListener('change', async(event) => {
-    event.preventDefault();
-    // ...
-
+// Use image only to prompt gemini
+filePicker.addEventListener("change", async (event) => {
+  event.preventDefault();
+  try {
     // Converts a File object to a GoogleGenerativeAI.Part object.
     async function fileToGenerativePart(file) {
-        const base64EncodedDataPromise = new Promise((resolve) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result.split(',')[1]);
-            reader.readAsDataURL(file);
-        });
-        return {
-            inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
-        };
+      const base64EncodedDataPromise = new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result.split(",")[1]);
+        reader.readAsDataURL(file);
+      });
+      return {
+        inlineData: {
+          data: await base64EncodedDataPromise,
+          mimeType: file.type,
+        },
+      };
     }
 
-    async function run() {
-        // For text-and-images input (multimodal), use the gemini-pro-vision model
-        const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+    const askGemini = async () => {
+      // For text-and-images input (multimodal), use the gemini-pro-vision model
+      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+      const prompt = "What's different between these pictures?";
 
-        const prompt = "What's different between these pictures?";
+      const fileInputEl = document.querySelector("input[type=file]");
+      const imageParts = await Promise.all(
+        [...fileInputEl.files].map(fileToGenerativePart)
+      );
 
-        const fileInputEl = document.querySelector("input[type=file]");
-        const imageParts = await Promise.all(
-            [...fileInputEl.files].map(fileToGenerativePart)
-        );
+      const result = await model.generateContent([prompt, ...imageParts]);
+      const response = await result.response;
+      const text = response.text();
 
-        const result = await model.generateContent([prompt, ...imageParts]);
-        const response = await result.response;
-        const text = response.text();
-        message.innerText = text;
-        console.log(text);
-    }
-
-    await run();
+      message.innerText = text;
+    };
+    await askGemini();
     respDialog.show();
-})
-
-
-
-
+  } catch (error) {
+    const message = error?.message ?? "Something went wrong"
+    console.warn(message);
+    alert(message)
+  }
+});
